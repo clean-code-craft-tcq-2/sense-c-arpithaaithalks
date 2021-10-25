89aeec6bc95e64fb52ba08a2358add7a588bb02b
arpithaaithalks <91457365+arpithaaithalks@users.noreply.github.com>
2021-10-25 23:53:02 +0530

    Update alert.h

--- alert.h
+++ alert.h
@@ -1,7 +1,6 @@
-#include "stats.h"
 
 typedef void (*alerter_funcptr)();
-void check_and_alert(float maxThreshold, alerter_funcptr alerters[], Stats computedStats);
+void check_and_alert(float maxThreshold, alerter_funcptr alerters[], typedef struct Stats computedStats);
 void emailAlerter(void);
 void ledAlerter(void);
 
