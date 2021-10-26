43ba37d90faec9200eb7ca719b7e228b4c255501
arpithaaithalks <91457365+arpithaaithalks@users.noreply.github.com>
2021-10-26 10:19:54 +0530

    Update stats.h

--- stats.h
+++ stats.h
@@ -13,3 +13,6 @@ void check_and_alert(float maxThreshold, alerter_funcptr alerters[], struct Stat
 extern struct Stats computedStats;
 
 Stats compute_statistics(const float* numberset, int setlength);
+
+extern int emailAlertCallCount;
+extern int ledAlertCallCount;
