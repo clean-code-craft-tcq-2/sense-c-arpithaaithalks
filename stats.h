19e5f8cfebf207121b8969d0a0f05a6f66c1ab4b
arpithaaithalks <91457365+arpithaaithalks@users.noreply.github.com>
2021-10-26 09:59:45 +0530

    Update stats.h

--- stats.h
+++ stats.h
@@ -6,6 +6,10 @@ struct Stats
   float min;
 };
 
+
+typedef void (*alerter_funcptr)();
+void check_and_alert(float maxThreshold, alerter_funcptr alerters[], struct Stats computedStats);
+
 extern struct Stats computedStats;
 
 Stats compute_statistics(const float* numberset, int setlength);
